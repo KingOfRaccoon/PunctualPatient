735f6a92ac888bc1ca5ccd375b499c5ac3545c7b
polina1441 <14042003dp@mail.ru>
2020-12-03 17:38:10 +0700

    list

--- app/src/main/java/com/kingofraccoon/punctualpatient/fragment/ProfileFragment.kt
+++ app/src/main/java/com/kingofraccoon/punctualpatient/fragment/ProfileFragment.kt
@@ -4,13 +4,72 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.kingofraccoon.punctualpatient.R
+import com.tutorialwing.expandablelistview.ProfileExpandableListAdapter
 
 class ProfileFragment: Fragment() {
+
+    internal var adapter: ExpandableListAdapter ?= null
+    internal var titleList: List<String> ?= null
+
+    val data: HashMap<String, List<String>>
+        get() {
+            val listData = HashMap<String, List<String>>()
+
+            val redmiMobiles = ArrayList<String>()
+            redmiMobiles.add("Redmi Y2")
+            redmiMobiles.add("Redmi S2")
+            redmiMobiles.add("Redmi Note 5 Pro")
+            redmiMobiles.add("Redmi Note 5")
+            redmiMobiles.add("Redmi 5 Plus")
+            redmiMobiles.add("Redmi Y1")
+            redmiMobiles.add("Redmi 3S Plus")
+
+            listData["Redmi"] = redmiMobiles
+
+            return listData
+        }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.profile_fragment, container, false)
 
+        val expandableListView = view.findViewById<ExpandableListView>(R.id.expandableListView)
+        if (expandableListView != null) {
+            val listData = data
+            titleList = ArrayList(listData.keys)
+            adapter = ProfileExpandableListAdapter(requireContext(), titleList as ArrayList<String>, listData)
+            expandableListView!!.setAdapter(adapter)
+            expandableListView!!.setOnGroupExpandListener { groupPosition ->
+                Toast.makeText(
+                    activity?.applicationContext,
+                    (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+                Toast.makeText(
+                    activity?.applicationContext,
+                    (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            expandableListView!!.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+                Toast.makeText(
+                    activity?.applicationContext,
+                    "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(titleList as ArrayList<String>)[groupPosition]]!!.get(
+                        childPosition
+                    ),
+                    Toast.LENGTH_SHORT
+                ).show()
+                false
+            }
+        }
         return view
     }
 }
